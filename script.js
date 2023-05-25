c36d07f557c16f1b85618bf165690f30c7a02d2d
michaeljamil <michaeljamilmangga.cervano@bicol-u.edu.ph>
2023-05-26 03:51:26 +0800

    Update

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const navLinks = document.querySelectorAll("nav ul li a");
-
-  navLinks.forEach(function(link) {
-    link.addEventListener("click", navigateToPage);
+  const progressBars = document.querySelectorAll(".progress-bar");
+  
+  progressBars.forEach(function(progressBar) {
+    const progress = progressBar.getAttribute("data-progress");
+    progressBar.style.animation = `progress-bar ${progress} linear forwards`;
   });
-
-  function navigateToPage(e) {
-    e.preventDefault();
-
-    const pageURL = this.getAttribute("href");
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", pageURL, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        const responseHTML = xhr.responseText;
-        const parser = new DOMParser();
-        const newDocument = parser.parseFromString(responseHTML, "text/html");
-
-        document.documentElement.innerHTML = newDocument.documentElement.innerHTML;
-        history.pushState({}, "", pageURL);
-
-        // Run scripts after loading the new content
-        const scripts = document.getElementsByTagName("script");
-        for (let i = 0; i < scripts.length; i++) {
-          eval(scripts[i].text);
-        }
-      }
-    };
-    xhr.send();
-  }
 });
