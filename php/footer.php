9e60cf998fc167661d4ca152a74e43db02c9459e
michaeljamil <michaeljamilmangga.cervano@bicol-u.edu.ph>
2023-05-26 11:42:05 +0800

    Update

--- php/footer.php
+++ php/footer.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Example PHP Footer</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-    <footer>
-        <p>Michael Jamil M. Cervaño</p>
-        <p>For More HTML, CSS</p>
-        <div class="social">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-        </div>
-        <p>Copyright by Michael Jamil M. Cervaño <?php echo date('Y'); ?></p>
-    </footer>
-</body>
-</html>
+<?php
+$currentYear = date('Y');
+echo "&copy; $currentYear";
+?>
